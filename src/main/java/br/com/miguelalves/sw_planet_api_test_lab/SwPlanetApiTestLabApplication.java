bad85f590da2c1e4aa4b2699c6f4a5681c8e5495
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-17 17:15:46 -0300

    chore: add inital project setup

---
+++ src/main/java/br/com/miguelalves/sw_planet_api_test_lab/SwPlanetApiTestLabApplication.java
@@ -0,0 +1,13 @@
+package br.com.miguelalves.sw_planet_api_test_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwPlanetApiTestLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwPlanetApiTestLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/miguelalves/sw_planet_api_test_lab/SwPlanetApiTestLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miguelalves.sw_planet_api_test_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwPlanetApiTestLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
