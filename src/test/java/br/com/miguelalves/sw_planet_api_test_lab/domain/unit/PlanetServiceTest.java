3148a10404f9adf44a38e25c1d0d538c860d59fd
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-17 19:59:03 -0300

    test: add unit tests for get planet by id method

--- src/test/java/br/com/miguelalves/sw_planet_api_test_lab/domain/unit/PlanetServiceTest.java
+++ src/test/java/br/com/miguelalves/sw_planet_api_test_lab/domain/unit/PlanetServiceTest.java
@@ -1,9 +1,12 @@
-package br.com.miguelalves.sw_planet_api_test_lab.domain;
+package br.com.miguelalves.sw_planet_api_test_lab.domain.unit;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.anyLong;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
@@ -11,6 +14,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static br.com.miguelalves.sw_planet_api_test_lab.common.PlanetConstants.INVALID_PLANET;
 import static br.com.miguelalves.sw_planet_api_test_lab.common.PlanetConstants.PLANET;
+import br.com.miguelalves.sw_planet_api_test_lab.domain.Planet;
+import br.com.miguelalves.sw_planet_api_test_lab.domain.PlanetRepository;
+import br.com.miguelalves.sw_planet_api_test_lab.domain.PlanetService;
 
 @ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
@@ -32,5 +38,20 @@ public class PlanetServiceTest {
     public void createPlanet_WithInvalidData_ThrowsException() {
         when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
         assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void getPlanet_WithValidId_ReturnsPlanet() {
+        when(planetRepository.findById(anyLong())).thenReturn(Optional.of(PLANET));
+        Optional<Planet> sut = planetService.get(1L);
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void getPlanet_ByUnexistingId_ReturnsEmpty() {
+        when(planetRepository.findById(anyLong())).thenReturn(Optional.empty());
+        Optional<Planet> sut = planetService.get(1L);
+        assertThat(sut).isEmpty();
     }
 }
