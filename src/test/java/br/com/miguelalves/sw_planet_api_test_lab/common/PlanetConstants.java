dd0e35ed4d4fd027f871ed1c0a69e7fcef59e960
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-17 19:44:30 -0300

    test: ensure PlanetService throw an error if invalid data is passed

--- src/test/java/br/com/miguelalves/sw_planet_api_test_lab/common/PlanetConstants.java
+++ src/test/java/br/com/miguelalves/sw_planet_api_test_lab/common/PlanetConstants.java
@@ -4,4 +4,5 @@ import br.com.miguelalves.sw_planet_api_test_lab.domain.Planet;
 
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
+    public static final Planet INVALID_PLANET = new Planet("", "", "");
 }
--- src/test/java/br/com/miguelalves/sw_planet_api_test_lab/domain/PlanetServiceTest.java
+++ src/test/java/br/com/miguelalves/sw_planet_api_test_lab/domain/PlanetServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.miguelalves.sw_planet_api_test_lab.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,6 +9,7 @@ import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static br.com.miguelalves.sw_planet_api_test_lab.common.PlanetConstants.INVALID_PLANET;
 import static br.com.miguelalves.sw_planet_api_test_lab.common.PlanetConstants.PLANET;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,5 +26,11 @@ public class PlanetServiceTest {
         when(planetRepository.save(PLANET)).thenReturn(PLANET);
         Planet sut = planetService.create(PLANET);
         assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
     }
 }
