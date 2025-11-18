1b92428ced24999dad73aff9bfd2d3ebc757c2ab
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-18 19:39:18 -0300

    test: add unit tests for ist all planets with filtering method

--- src/test/java/br/com/miguelalves/sw_planet_api_test_lab/domain/unit/PlanetServiceTest.java
+++ src/test/java/br/com/miguelalves/sw_planet_api_test_lab/domain/unit/PlanetServiceTest.java
@@ -1,22 +1,28 @@
 package br.com.miguelalves.sw_planet_api_test_lab.domain.unit;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
 
 import static br.com.miguelalves.sw_planet_api_test_lab.common.PlanetConstants.INVALID_PLANET;
 import static br.com.miguelalves.sw_planet_api_test_lab.common.PlanetConstants.PLANET;
 import br.com.miguelalves.sw_planet_api_test_lab.domain.Planet;
 import br.com.miguelalves.sw_planet_api_test_lab.domain.PlanetRepository;
 import br.com.miguelalves.sw_planet_api_test_lab.domain.PlanetService;
+import br.com.miguelalves.sw_planet_api_test_lab.domain.QueryBuilder;
 
 @ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
@@ -66,6 +72,26 @@ public class PlanetServiceTest {
         final String name = "Unexisting Planet";
         when(planetRepository.findByName(name)).thenReturn(Optional.empty());
         Optional<Planet> sut = planetService.getByName(name);
+        assertThat(sut).isEmpty();
+    }
+
+    @Test
+    public void listPlanets_ReturnsAllPlanets() {
+        List<Planet> planets = new ArrayList<>() {{
+            add(PLANET);
+        }};
+        Example <Planet> query = QueryBuilder.makeQuery(new Planet(PLANET.getClimate(), PLANET.getTerrain()));
+        when(planetRepository.findAll(query)).thenReturn(planets);
+        List<Planet> sut = planetService.list(PLANET.getTerrain(), PLANET.getClimate());
+        assertThat(sut).isNotEmpty();
+        assertThat(sut).hasSize(1);
+        assertThat(sut.get(0)).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void listPlanets_ReturnsNoPlanets() {
+        when(planetRepository.findAll(any())).thenReturn(Collections.emptyList());
+        List<Planet> sut = planetService.list(PLANET.getClimate(), PLANET.getTerrain());
         assertThat(sut).isEmpty();
     }
 }
