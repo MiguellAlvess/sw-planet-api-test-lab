fca5d85c03d7cbfddc53e001246bb6d703eaa919
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-19 10:09:46 -0300

    test: ensure that the planet cannot be created with invalid data in the database

--- src/test/java/br/com/miguelalves/sw_planet_api_test_lab/domain/integration/PlanetRespositoryTest.java
+++ src/test/java/br/com/miguelalves/sw_planet_api_test_lab/domain/integration/PlanetRespositoryTest.java
@@ -1,6 +1,7 @@
 package br.com.miguelalves.sw_planet_api_test_lab.domain.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -27,5 +28,13 @@ public class PlanetRespositoryTest {
         assertThat(sut.getName()).isEqualTo(PLANET.getName());
         assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
         assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        Planet emptyPlanet = new Planet();
+        Planet invalidPlanet = new Planet("", "", "");
+        assertThatThrownBy(() -> planetRepository.save(emptyPlanet)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> planetRepository.save(invalidPlanet)).isInstanceOf(RuntimeException.class);
     }
 }
