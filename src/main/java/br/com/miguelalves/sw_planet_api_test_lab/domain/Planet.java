899b11544413af54ba83d8d56130baabd5c33707
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-19 09:48:03 -0300

    feat: add get by id method to Planet entity

--- src/main/java/br/com/miguelalves/sw_planet_api_test_lab/domain/Planet.java
+++ src/main/java/br/com/miguelalves/sw_planet_api_test_lab/domain/Planet.java
@@ -29,6 +29,10 @@ public class Planet {
         this.terrain = terrain;
     }    
 
+    public Long getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
